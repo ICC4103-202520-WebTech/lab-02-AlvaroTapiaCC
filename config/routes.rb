c60b4d6bfb3c19af2e233b7d938cb0d93969b1c6
Alvaro Tapia <ajtapia@miuandes.com>
2025-08-21 17:57:49 -0400

    check fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root "aboutme#index"
   get "aboutme", to: "aboutme#index"
   get "up" => "rails/health#show", as: :rails_health_check
-
 end
--- test/controllers/aboutme_controller_test.rb
+++ test/controllers/aboutme_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class AboutControllerTest < ActionDispatch::IntegrationTest
+class AboutmeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get about_index_url
+    get aboutme_url
     assert_response :success
   end
 end
